37169cc78f8add43fce589b01444b8aade4985cf
nadissu <nadidesigirtmac@gmail.com>
2022-09-22 01:25:14 +0300

    RegisterUserDto Refactoring

--- src/kodlamaIoDevs/Kodlama.io.Devs.Application/Features/Auth/Dtos/RegisterUserDto.cs
+++ src/kodlamaIoDevs/Kodlama.io.Devs.Application/Features/Auth/Dtos/RegisterUserDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Security.Dtos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +7,8 @@ using System.Threading.Tasks;
 
 namespace Kodlama.io.Devs.Application.Features.Auth.Dtos
 {
-    public class RegisterUserDto
+    public class RegisterUserDto:UserForRegisterDto
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string Token { get; set; }
+        
     }
 }
